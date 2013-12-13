9ba194dd2ed616048f3553034b3874dde227d327
Max Barkley <mbarkley@redhat.com>
2013-12-13 10:54:43 -0500

    Add artifact name constants.

---
+++ src/main/java/org/jboss/errai/forge/constant/ErraiArtifact.java
@@ -0,0 +1,32 @@
+package org.jboss.errai.forge.constant;
+
+
+public final class ErraiArtifact {
+  
+  public static final String GROUP_ID = "org.jboss.errai";
+  
+  public static enum ArtifactId {
+    ErraiCommon("errai-common");
+    
+    private final String artifactId;
+    private final String groupId;
+    
+    private ArtifactId(final String artifactId, final String groupId) {
+      this.artifactId = artifactId;
+      this.groupId = groupId;
+    }
+
+    private ArtifactId(final String id) {
+      this(id, GROUP_ID);
+    }
+    
+    public String getArtifactId() {
+      return artifactId;
+    }
+    
+    public String getGroupId() {
+      return groupId;
+    }
+  }
+
+}
