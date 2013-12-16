9c6a4c4dc066e1dd45cb867e9616a60c89cb776e
Max Barkley <mbarkley@redhat.com>
2013-12-16 16:35:51 -0500

    Add new required facets to errai build facet.

--- src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBuildDependencyFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBuildDependencyFacet.java
@@ -6,27 +6,26 @@ import org.jboss.errai.forge.facet.plugin.CleanPluginFacet;
 import org.jboss.errai.forge.facet.plugin.CompilerPluginFacet;
 import org.jboss.errai.forge.facet.plugin.DependencyPluginFacet;
 import org.jboss.errai.forge.facet.plugin.GwtPluginFacet;
+import org.jboss.errai.forge.facet.plugin.JbossPluginFacet;
+import org.jboss.errai.forge.facet.plugin.WarPluginFacet;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.dependencies.ScopeType;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
-@RequiresFacet({CleanPluginFacet.class, CompilerPluginFacet.class, DependencyPluginFacet.class, GwtPluginFacet.class})
+@RequiresFacet({ CleanPluginFacet.class, CompilerPluginFacet.class, DependencyPluginFacet.class, GwtPluginFacet.class,
+    JbossPluginFacet.class, WarPluginFacet.class })
 public class ErraiBuildDependencyFacet extends AbstractDependencyFacet {
 
   public ErraiBuildDependencyFacet() {
-    setCoreDependencies(
-            DependencyBuilder.create(ErraiTools.toString()),
-            DependencyBuilder.create(GwtUser.toString()).setScopeType(ScopeType.PROVIDED),
-            DependencyBuilder.create(ErraiJboss.toString()),
-            DependencyBuilder.create(JUnit.toString()).setScopeType(ScopeType.TEST)
-    );
-    setProfileDependencies(PRODUCTION_PROFILE, 
+    setCoreDependencies(DependencyBuilder.create(ErraiTools.toString()), DependencyBuilder.create(GwtUser.toString())
+            .setScopeType(ScopeType.PROVIDED), DependencyBuilder.create(ErraiJboss.toString()), DependencyBuilder
+            .create(JUnit.toString()).setScopeType(ScopeType.TEST));
+    setProfileDependencies(PRODUCTION_PROFILE,
             DependencyBuilder.create(ErraiTools.toString()).setScopeType(ScopeType.PROVIDED),
             DependencyBuilder.create(ErraiJboss.toString()).setScopeType(ScopeType.PROVIDED),
             DependencyBuilder.create(Hsq.toString()).setScopeType(ScopeType.PROVIDED),
             DependencyBuilder.create(JUnit.toString()).setScopeType(ScopeType.PROVIDED),
-            DependencyBuilder.create(ErraiNetty.toString()).setScopeType(ScopeType.PROVIDED)
-            );
+            DependencyBuilder.create(ErraiNetty.toString()).setScopeType(ScopeType.PROVIDED));
   }
-  
+
 }
