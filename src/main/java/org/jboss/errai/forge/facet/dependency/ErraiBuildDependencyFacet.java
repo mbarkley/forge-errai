b320de8e09f126106ce262cc1f6384a3a6692b23
Max Barkley <mbarkley@redhat.com>
2013-12-16 14:30:26 -0500

    Add required plugin facets to ErraiBuildDependencyFacet.

--- src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBuildDependencyFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBuildDependencyFacet.java
@@ -2,9 +2,15 @@ package org.jboss.errai.forge.facet.dependency;
 
 import static org.jboss.errai.forge.constant.ArtifactVault.DependencyArtifact.*;
 
+import org.jboss.errai.forge.facet.plugin.CleanPluginFacet;
+import org.jboss.errai.forge.facet.plugin.CompilerPluginFacet;
+import org.jboss.errai.forge.facet.plugin.DependencyPluginFacet;
+import org.jboss.errai.forge.facet.plugin.GwtPluginFacet;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.dependencies.ScopeType;
+import org.jboss.forge.shell.plugins.RequiresFacet;
 
+@RequiresFacet({CleanPluginFacet.class, CompilerPluginFacet.class, DependencyPluginFacet.class, GwtPluginFacet.class})
 public class ErraiBuildDependencyFacet extends AbstractDependencyFacet {
 
   public ErraiBuildDependencyFacet() {
