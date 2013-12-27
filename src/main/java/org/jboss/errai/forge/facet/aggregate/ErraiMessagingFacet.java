6771981584e3d32343d973569332774e7404131a
Max Barkley <mbarkley@redhat.com>
2013-12-27 14:55:14 -0500

    Add errai messaging facet.

---
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiMessagingFacet.java
@@ -0,0 +1,12 @@
+package org.jboss.errai.forge.facet.aggregate;
+
+import org.jboss.forge.shell.plugins.RequiresFacet;
+
+/**
+ * Aggregator facet for Errai Messaging.
+ * 
+ * @author Max Barkley <mbarkley@redhat.com>
+ */
+@RequiresFacet({CoreFacet.class})
+public class ErraiMessagingFacet extends BaseAggregatorFacet {
+}
