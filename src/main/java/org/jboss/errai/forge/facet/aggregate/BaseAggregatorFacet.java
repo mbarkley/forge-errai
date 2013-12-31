8cceab8b314efb290022019076b946cd23edd5da
Max Barkley <mbarkley@redhat.com>
2013-12-31 16:54:18 -0500

    Change isInstalled to true if direct requirements are installed in aggregators.

--- src/main/java/org/jboss/errai/forge/facet/aggregate/BaseAggregatorFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/BaseAggregatorFacet.java
@@ -1,6 +1,13 @@
 package org.jboss.errai.forge.facet.aggregate;
 
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.jboss.forge.project.Facet;
 import org.jboss.forge.project.facets.BaseFacet;
+import org.jboss.forge.shell.plugins.RequiresFacet;
 
 /**
  * Acts as top-level aggregator for pulling in other facet dependencies.
@@ -10,6 +17,9 @@ import org.jboss.forge.project.facets.BaseFacet;
 abstract class BaseAggregatorFacet extends BaseFacet {
   // TODO Implement uninstall
 
+  @Inject
+  private BeanManager bm;
+  
   @Override
   public boolean install() {
     return true;
@@ -17,7 +27,27 @@ abstract class BaseAggregatorFacet extends BaseFacet {
 
   @Override
   public boolean isInstalled() {
-    return getProject().hasFacet(getClass());
+    if (getProject().hasFacet(getClass())) {
+      return true;
+    }
+    else {
+      /*
+       * An aggregator facet is installed if all of its required facets are
+       * installed.
+       */
+      final Class<? extends Facet>[] directDependencies = getClass().getAnnotation(RequiresFacet.class).value();
+      for (final Class<? extends Facet> facetType : directDependencies) {
+        @SuppressWarnings("unchecked")
+        final Bean<? extends Facet> bean = (Bean<? extends Facet>) bm.getBeans(facetType).iterator().next();
+        final CreationalContext<? extends Facet> ctx = bm.createCreationalContext(bean);
+        Facet facet = (Facet) bm.getReference(bean, facetType, ctx);
+        if (!facet.isInstalled()) {
+          return false;
+        }
+      }
+      
+      return true;
+    }
   }
 
 }
