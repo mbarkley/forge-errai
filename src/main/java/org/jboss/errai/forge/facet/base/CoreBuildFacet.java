d43f466f5494f886ae47d1aac3f959f28ce9cf82
Max Barkley <mbarkley@redhat.com>
2014-01-06 14:33:42 -0500

    Implement isInstalled in CoreBuildFacet.

--- src/main/java/org/jboss/errai/forge/facet/base/CoreBuildFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/base/CoreBuildFacet.java
@@ -1,6 +1,7 @@
 package org.jboss.errai.forge.facet.base;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Model;
@@ -11,7 +12,7 @@ import org.jboss.forge.maven.MavenCoreFacet;
 import org.jboss.forge.project.facets.DependencyFacet;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
-@RequiresFacet({MavenCoreFacet.class, DependencyFacet.class})
+@RequiresFacet({ MavenCoreFacet.class, DependencyFacet.class })
 public class CoreBuildFacet extends AbstractBaseFacet {
 
   public final String javaSrcPath = "src/main/java";
@@ -20,31 +21,47 @@ public class CoreBuildFacet extends AbstractBaseFacet {
 
   @Override
   public boolean install() {
-      final MavenCoreFacet coreFacet = getProject().getFacet(MavenCoreFacet.class);
-      final Model pom = coreFacet.getPOM();
-      final Build build = pom.getBuild();
-      
-      build.setOutputDirectory(buildOutput);
-      // TODO don't hardcode path
-      pom.addProperty(Property.JbossHome.getName(), "${project.build.directory}/jboss-as-7.1.1.Final");
-      pom.addProperty(Property.DevContext.getName(), "${project.artifactId}");
-      pom.addProperty(Property.ErraiVersion.getName(), new VersionOracle(getProject().getFacet(DependencyFacet.class)).resolveErraiVersion());
-      
-      Resource res = getResource(javaSrcPath, build.getResources());
-      if (res == null) {
-        res = new Resource();
-        res.setDirectory(javaSrcPath);
-        build.addResource(res);
-      }
-      res = getResource(resSrcPath, build.getResources());
-      if (res == null) {
-        res = new Resource();
-        res.setDirectory(resSrcPath);
-        build.addResource(res);
-      }
-      coreFacet.setPOM(pom);
-      
-      return true;
+    final MavenCoreFacet coreFacet = getProject().getFacet(MavenCoreFacet.class);
+    final Model pom = coreFacet.getPOM();
+    final Build build = pom.getBuild();
+
+    build.setOutputDirectory(buildOutput);
+    // TODO don't hardcode path
+    pom.addProperty(Property.JbossHome.getName(), "${project.build.directory}/jboss-as-7.1.1.Final");
+    pom.addProperty(Property.DevContext.getName(), "${project.artifactId}");
+    pom.addProperty(Property.ErraiVersion.getName(),
+            new VersionOracle(getProject().getFacet(DependencyFacet.class)).resolveErraiVersion());
+
+    Resource res = getResource(javaSrcPath, build.getResources());
+    if (res == null) {
+      res = new Resource();
+      res.setDirectory(javaSrcPath);
+      build.addResource(res);
+    }
+    res = getResource(resSrcPath, build.getResources());
+    if (res == null) {
+      res = new Resource();
+      res.setDirectory(resSrcPath);
+      build.addResource(res);
+    }
+    coreFacet.setPOM(pom);
+
+    return true;
+  }
+
+  @Override
+  public boolean isInstalled() {
+    final MavenCoreFacet coreFacet = getProject().getFacet(MavenCoreFacet.class);
+    final Model pom = coreFacet.getPOM();
+    final Build build = pom.getBuild();
+
+    Properties properties = pom.getProperties();
+    return !(build == null || build.getOutputDirectory() == null || !build.getOutputDirectory().equals(buildOutput)
+            || !properties.containsKey(Property.JbossHome.getName())
+            || !properties.containsKey(Property.DevContext.getName())
+            || !properties.containsKey(Property.ErraiVersion.getName())
+            || getResource(javaSrcPath, build.getResources()) == null
+            || getResource(resSrcPath, build.getResources()) == null);
   }
 
   private Resource getResource(String relPath, List<Resource> resources) {
@@ -52,7 +69,7 @@ public class CoreBuildFacet extends AbstractBaseFacet {
       if (res.getDirectory().endsWith(relPath))
         return res;
     }
-    
+
     return null;
   }
 }
