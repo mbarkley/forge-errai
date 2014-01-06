d93f736f28837954b9197be66855ea806a83458a
Max Barkley <mbarkley@redhat.com>
2014-01-06 15:20:37 -0500

    Fix isInstalled in BaseAggregatorFacet.

--- src/main/java/org/jboss/errai/forge/facet/aggregate/BaseAggregatorFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/BaseAggregatorFacet.java
@@ -1,11 +1,5 @@
 package org.jboss.errai.forge.facet.aggregate;
 
-import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
-
-import org.jboss.forge.project.Facet;
 import org.jboss.forge.project.facets.BaseFacet;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
@@ -17,9 +11,6 @@ import org.jboss.forge.shell.plugins.RequiresFacet;
 abstract class BaseAggregatorFacet extends BaseFacet {
   // TODO Implement uninstall
 
-  @Inject
-  private BeanManager bm;
-  
   @Override
   public boolean install() {
     return true;
@@ -27,27 +18,11 @@ abstract class BaseAggregatorFacet extends BaseFacet {
 
   @Override
   public boolean isInstalled() {
-    if (getProject().hasFacet(getClass())) {
-      return true;
-    }
-    else {
-      /*
-       * An aggregator facet is installed if all of its required facets are
-       * installed.
-       */
-      final Class<? extends Facet>[] directDependencies = getClass().getAnnotation(RequiresFacet.class).value();
-      for (final Class<? extends Facet> facetType : directDependencies) {
-        @SuppressWarnings("unchecked")
-        final Bean<? extends Facet> bean = (Bean<? extends Facet>) bm.getBeans(facetType).iterator().next();
-        final CreationalContext<? extends Facet> ctx = bm.createCreationalContext(bean);
-        Facet facet = (Facet) bm.getReference(bean, facetType, ctx);
-        if (!facet.isInstalled()) {
-          return false;
-        }
-      }
-      
-      return true;
-    }
+    /*
+     * An aggregator facet is installed if all of its required facets are
+     * installed.
+     */
+    return getProject().hasAllFacets(getClass().getAnnotation(RequiresFacet.class).value());
   }
 
 }
