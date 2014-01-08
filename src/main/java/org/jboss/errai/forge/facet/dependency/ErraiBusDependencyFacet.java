131c41b07c06aa4d1fbc776cf50d84b417a067b1
Max Barkley <mbarkley@redhat.com>
2014-01-08 16:38:31 -0500

    Guava should not have been provided scoped dependency.

--- src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBusDependencyFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/dependency/ErraiBusDependencyFacet.java
@@ -1,10 +1,11 @@
 package org.jboss.errai.forge.facet.dependency;
 
 import static org.jboss.errai.forge.constant.ArtifactVault.DependencyArtifact.ErraiBus;
-import static org.jboss.errai.forge.constant.ArtifactVault.DependencyArtifact.Guava;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 import org.jboss.forge.project.dependencies.DependencyBuilder;
-import org.jboss.forge.project.dependencies.ScopeType;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
 @RequiresFacet({ ErraiCommonDepdencyFacet.class })
@@ -12,7 +13,7 @@ public class ErraiBusDependencyFacet extends AbstractDependencyFacet {
 
   public ErraiBusDependencyFacet() {
     setCoreDependencies(DependencyBuilder.create(ErraiBus.toString()));
-    setProfileDependencies(MAIN_PROFILE, DependencyBuilder.create(Guava.toString()).setScopeType(ScopeType.PROVIDED));
+    profileDependencies = new HashMap<String, Collection<DependencyBuilder>>();
   }
 
 }
