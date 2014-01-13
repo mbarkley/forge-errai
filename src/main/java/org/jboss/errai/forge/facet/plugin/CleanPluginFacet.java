e04a68a937a37e701c40a03e777489da1edc6c46
Max Barkley <mbarkley@redhat.com>
2014-01-13 17:28:34 -0500

    Fix NPE in clean plugin facet.

--- src/main/java/org/jboss/errai/forge/facet/plugin/CleanPluginFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/plugin/CleanPluginFacet.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 
 import javax.inject.Inject;
 
-import org.jboss.errai.forge.config.ProjectConfigFactory;
 import org.jboss.errai.forge.config.ProjectConfig.ProjectProperty;
+import org.jboss.errai.forge.config.ProjectConfigFactory;
 import org.jboss.errai.forge.constant.ArtifactVault.DependencyArtifact;
 import org.jboss.errai.forge.facet.base.CoreBuildFacet;
 import org.jboss.forge.maven.plugins.ConfigurationElement;
 import org.jboss.forge.maven.plugins.ConfigurationElementBuilder;
 import org.jboss.forge.maven.plugins.Execution;
+import org.jboss.forge.project.Project;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
@@ -27,13 +28,12 @@ public class CleanPluginFacet extends AbstractPluginFacet {
   private @Inject
   ProjectConfigFactory factory;
 
+  private boolean moduleAdded = false;
+
   public CleanPluginFacet() {
     pluginArtifact = DependencyArtifact.Clean;
     dependencies = new ArrayList<DependencyBuilder>(0);
     executions = new ArrayList<Execution>(0);
-
-    final String moduleName = factory.getProjectConfig(getProject()).getProjectProperty(ProjectProperty.MODULE_LOGICAL,
-            String.class);
 
     configurations = Arrays.asList(new ConfigurationElement[] {
         ConfigurationElementBuilder.create().setName("filesets").addChild(
@@ -42,8 +42,6 @@ public class CleanPluginFacet extends AbstractPluginFacet {
                         .addChild(ConfigurationElementBuilder.create().setName("includes")
                                 .addChild(ConfigurationElementBuilder.create().setName("include")
                                         .setText("src/main/webapp/WEB-INF/deploy/"))
-                                .addChild(ConfigurationElementBuilder.create().setName("include")
-                                        .setText("src/main/webapp/" + moduleName))
                                 .addChild(ConfigurationElementBuilder.create().setName("include")
                                         .setText("src/main/webapp/WEB-INF/lib/"))
                                 .addChild(ConfigurationElementBuilder.create().setName("include")
@@ -55,4 +53,18 @@ public class CleanPluginFacet extends AbstractPluginFacet {
     });
   }
 
+  @Override
+  public void setProject(Project project) {
+    super.setProject(project);
+
+    if (!moduleAdded) {
+      final String moduleName = factory.getProjectConfig(getProject()).getProjectProperty(
+              ProjectProperty.MODULE_LOGICAL,
+              String.class);
+      ((ConfigurationElementBuilder) configurations.iterator().next().getChildren().get(0))
+              .addChild(ConfigurationElementBuilder.create().setName("include")
+                      .setText("src/main/webapp/" + moduleName));
+      moduleAdded = true;
+    }
+  }
 }
