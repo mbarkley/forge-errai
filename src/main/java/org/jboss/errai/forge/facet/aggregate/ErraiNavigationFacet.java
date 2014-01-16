d7dc0a253b05b9fe43700c67739e87e2632a23ee
Max Barkley <mbarkley@redhat.com>
2014-01-16 12:24:19 -0500

    Add errai-navigation dependency facet as requirement for aggregator.

--- src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiNavigationFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiNavigationFacet.java
@@ -1,7 +1,8 @@
 package org.jboss.errai.forge.facet.aggregate;
 
+import org.jboss.errai.forge.facet.dependency.ErraiNavigationDependencyFacet;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
-@RequiresFacet({ CoreFacet.class, ErraiUiFacet.class })
+@RequiresFacet({ CoreFacet.class, ErraiUiFacet.class, ErraiNavigationDependencyFacet.class })
 public class ErraiNavigationFacet extends BaseAggregatorFacet {
 }
