f6b386fd6799721de76d8c53c14e4acd91bdef13
Max Barkley <mbarkley@redhat.com>
2014-01-16 11:38:01 -0500

    Add errai-ioc as requirement of errai-ui facet.

--- src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiUiFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiUiFacet.java
@@ -3,6 +3,6 @@ package org.jboss.errai.forge.facet.aggregate;
 import org.jboss.errai.forge.facet.dependency.ErraiUiDependencyFacet;
 import org.jboss.forge.shell.plugins.RequiresFacet;
 
-@RequiresFacet({ CoreFacet.class, ErraiUiDependencyFacet.class })
+@RequiresFacet({ CoreFacet.class, ErraiUiDependencyFacet.class, ErraiIocFacet.class })
 public class ErraiUiFacet extends BaseAggregatorFacet {
 }
