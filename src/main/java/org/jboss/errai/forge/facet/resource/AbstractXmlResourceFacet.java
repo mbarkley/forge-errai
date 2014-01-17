853e49f3dd54bebf677a1218f6dc43b888f9b643
Max Barkley <mbarkley@redhat.com>
2014-01-17 16:08:25 -0500

    Add documentation and remove unused methods from AbstractXmlResourceFacet.

--- src/main/java/org/jboss/errai/forge/facet/resource/AbstractXmlResourceFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/resource/AbstractXmlResourceFacet.java
@@ -1,10 +1,7 @@
 package org.jboss.errai.forge.facet.resource;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -27,7 +24,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * A base class for modifying XML-based configuration files. Concrete subclasses
@@ -143,24 +139,47 @@ public abstract class AbstractXmlResourceFacet extends AbstractBaseFacet {
     return true;
   }
 
+  /**
+   * Find a matching child {@link Node} from a given parent node.
+   * 
+   * @param parent
+   *          The parent node of the children to be searched.
+   * @param inserted
+   *          The node to be matched against.
+   * @return Returns a node, {@code result}, such that
+   *         {@code result.getParentNode().isSameNode(parent) && matches(inserted, result)}
+   *         .
+   */
   protected Node getMatchingChild(final Node parent, final Node inserted) {
     for (int i = 0; i < parent.getChildNodes().getLength(); i++) {
       if (matches(inserted, parent.getChildNodes().item(i)))
         return parent.getChildNodes().item(i);
     }
-    
+
     return null;
   }
 
+  /**
+   * Check if a matching child {@link Node} from a given parent node exists.
+   * 
+   * @param parent
+   *          The parent node of the children to be searched.
+   * @param inserted
+   *          The node to be matched against.
+   * @return Returns true if there exists a node, {@code result}, such that
+   *         {@code result.getParentNode().isSameNode(parent) && matches(inserted, result)}
+   *         .
+   */
   protected boolean hasMatchingChild(final Node parent, final Node inserted) {
     return getMatchingChild(parent, inserted) != null;
   }
 
   /**
-   * Get a collection of nodes used to verify the installation of this facet.
-   * Nodes in this collection must match nodes in the XML resource if this facet
-   * is installed. If a subclass does not override this, the default value will
-   * be the nodes returned by
+   * Get a map of parent nodes to xml fragments used to verify the installation
+   * of this facet. Nodes in collections in the values of this map must match
+   * children of the node matching the xpath expression key if this facet is
+   * installed. If a subclass does not override this, the default value will be
+   * the map returned by
    * {@link AbstractXmlResourceFacet#getElementsToInsert(Document)
    * getElementsToInsert}.
    */
@@ -212,7 +231,17 @@ public abstract class AbstractXmlResourceFacet extends AbstractBaseFacet {
     return true;
   }
 
-  protected abstract Map<XPathExpression, Node> getRemovalMap(XPath xPath, Document doc)
+  /**
+   * Return a map of xpath expressions of nodes to be replaced to their
+   * replacements, used when this facet is uninstalled.
+   * 
+   * @param xPath
+   *          Used to generate {@link XPathExpression XPathExpressions}.
+   * @param doc
+   *          Used to generate or import {@link Node Nodes}.
+   * @return A map of xpath expressions to nodes, used to uninstall this facet.
+   */
+  protected abstract Map<XPathExpression, Node> getRemovalMap(final XPath xPath, final Document doc)
           throws ParserConfigurationException, XPathExpressionException;
 
   protected File getResFile(final String relPath) {
@@ -224,9 +253,11 @@ public abstract class AbstractXmlResourceFacet extends AbstractBaseFacet {
   }
 
   /**
-   * Get DOM elements to write to an XML configuration file. Concrete subclasses
-   * should return a collection of nodes that will be merged as children of the
-   * root element of the specified XML file.
+   * Get DOM nodes to write to an XML configuration file. Concrete subclasses
+   * should return a map with keys as parent nodes and values as collections of
+   * nodes that will be merged as children of the the nodes matched by the
+   * respective key values. Each key should only match a unique node (i.e. this
+   * is a one-to-many map).
    * 
    * @param doc
    *          The returned nodes should be created with or imported into this
@@ -249,7 +280,7 @@ public abstract class AbstractXmlResourceFacet extends AbstractBaseFacet {
    * @param xPath
    *          Used to generate xpath expressions for finding nodes to replace.
    * @return A map of {@link XPathExpression XPathExpressions} (for finding
-   *         nodes to replace), to collections of {@link Node Nodes} to add as
+   *         nodes to replace), to replacement {@link Node Nodes} to add as
    *         children.
    */
   protected abstract Map<XPathExpression, Node> getReplacements(final XPath xPath, final Document doc)
@@ -264,68 +295,6 @@ public abstract class AbstractXmlResourceFacet extends AbstractBaseFacet {
    *         configuration file.
    */
   protected abstract String getRelPath();
-
-  /**
-   * @return A map with key values of node names, mapping to collections of
-   *         nodes with the corresponding node name.
-   */
-  protected Map<String, Collection<Node>> mapElementsByTagName(Collection<Node> elements) {
-    final Map<String, Collection<Node>> retVal = new HashMap<String, Collection<Node>>();
-    for (final Node node : elements) {
-      if (!retVal.containsKey(node.getNodeName()))
-        retVal.put(node.getNodeName(), new ArrayList<Node>());
-      retVal.get(node.getNodeName()).add(node);
-    }
-
-    return retVal;
-  }
-
-  /**
-   * @return A map with key values of node names, mapping to collections of
-   *         nodes with the corresponding node name.
-   */
-  protected Map<String, Collection<Node>> mapElementsByTagName(final NodeList nodes) {
-    final List<Node> list = new ArrayList<Node>(nodes.getLength());
-    for (int i = 0; i < nodes.getLength(); i++) {
-      list.add(nodes.item(i));
-    }
-
-    return mapElementsByTagName(list);
-  }
-
-  /**
-   * Add a collection of nodes as children of another node. If a node in the
-   * collection matches a child of the parent node, do not add it.
-   * 
-   * @param parent
-   *          The node to add children to.
-   * @param namedNodes
-   *          A collection of nodes to add as children.
-   */
-  protected void safeBatchAdd(final Node parent, final Map<String, Collection<Node>> namedNodes) {
-    Map<String, List<Node>> existingMap = new HashMap<String, List<Node>>();
-    for (final String name : namedNodes.keySet()) {
-      existingMap.put(name, new ArrayList<Node>());
-    }
-    for (Node cur = parent.getFirstChild(); cur != null; cur = cur.getNextSibling()) {
-      if (namedNodes.containsKey(cur.getNodeName())) {
-        existingMap.get(cur.getNodeName()).add(cur);
-      }
-    }
-    for (final String name : namedNodes.keySet()) {
-      List<Node> existing = existingMap.get(name);
-      nodeLoop: for (final Node node : namedNodes.get(name)) {
-        for (final Node existingNode : existing) {
-          if (node.getNodeType() == Node.ELEMENT_NODE && matches(node, existingNode))
-            break nodeLoop;
-        }
-        Node before = null;
-        if (!existing.isEmpty())
-          before = existing.get(existing.size() - 1).getNextSibling();
-        parent.insertBefore(node, before);
-      }
-    }
-  }
 
   /**
    * Check if a node is consistent with another. A node, {@code other} is
