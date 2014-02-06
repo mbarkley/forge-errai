a5fd0ba80cf1ea47de4ada2f6cb64f1042c8ed60
Max Barkley <mbarkley@redhat.com>
2014-02-06 18:00:41 -0500

    Fix versions in pom and tests.

--- src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
+++ src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
@@ -18,9 +18,9 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public abstract class ForgeTest {
 
-  public static final String DEPENDENCY = "org.jboss.errai.forge:errai-forge-plugin";
-  public static final String VERSION = "1.0.0-SNAPSHOT";
-  public static final String FORGE_VERSION = "2.0.0.CR2";
+  public static final String DEPENDENCY = "org.jboss.errai.forge:errai-forge-addon";
+  public static final String VERSION = "2.0.0.Beta1";
+  public static final String FORGE_VERSION = "2.0.0.Final";
   public static final String ADDON_GROUP = "org.jboss.forge.addon";
 
   @Inject
