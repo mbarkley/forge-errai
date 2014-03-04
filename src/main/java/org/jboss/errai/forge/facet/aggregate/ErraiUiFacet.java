da92da966759ddb732422aac917e6644f6782e0a
Max Barkley <mbarkley@redhat.com>
2014-03-04 18:56:52 -0500

    Add databinding as dependency of errai-ui (quickfix).

--- src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiUiFacet.java
+++ src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiUiFacet.java
@@ -1,11 +1,19 @@
 package org.jboss.errai.forge.facet.aggregate;
 
+import org.jboss.errai.forge.facet.dependency.ErraiDataBindingDependencyFacet;
+
 import org.jboss.errai.forge.facet.dependency.ErraiUiDependencyFacet;
 import org.jboss.errai.forge.facet.module.ErraiUiModuleFacet;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 
+/*
+ * NOTE: We must depend on the ErraiDataBindingDependencyFacet because the errai
+ * ui module inherits from the databinding module, but the errai ui does not
+ * have a dependency (via maven) on databinding.
+ */
+
 @FacetConstraint({ CoreFacet.class, ErraiIocFacet.class, ErraiCdiClientFacet.class, ErraiUiDependencyFacet.class,
-    ErraiUiModuleFacet.class })
+    ErraiDataBindingDependencyFacet.class, ErraiUiModuleFacet.class })
 public class ErraiUiFacet extends BaseAggregatorFacet {
 
   @Override
